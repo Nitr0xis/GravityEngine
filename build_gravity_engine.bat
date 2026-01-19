2570439dcc7ac20adc3c9e4620f6251712aa8742
Nitr0xis <nils.dontot@icloud.com>
2026-01-19 21:08:27 +0100

    Delete build_gravity_engine.bat

--- build_gravity_engine.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python -m PyInstaller --onefile --noconsole ^
---icon="C:\Users\donto\Documents\Projects\Programming\GravityEngine\assets\icon.ico" ^
---name "GravityEngine" ^
---distpath "C:\Users\donto\Documents\Projects\Programming\GravityEngine\app" ^
-"C:\Users\donto\Documents\Projects\Programming\GravityEngine\src\gravity_engine.py"
-
-pause
